f4f9d2dbc322fba674cd98be9d84c964f1749e80
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-18 16:22:20 -0600

    Defined Vector class with constructors and accessors

---
+++ src/vector.cpp
@@ -0,0 +1,9 @@
+#include "vector.hpp"
+
+// Constructor
+Vector::Vector(const float x, const float y, const float z)
+    : m_x{ x }
+    , m_y{ y }
+    , m_z{ z }
+{
+};
---
+++ src/vector.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+class Vector
+{
+public:
+    // Constructors
+    Vector() = default;
+    Vector(float x, float y, float z);
+    Vector(Vector&) = default;
+
+    // Destructor
+    ~Vector() = default;
+
+    // Accessors
+    float x() const { return m_x; }
+    float y() const { return m_y; }
+    float z() const { return m_z; }
+    float w() const { return m_w; }
+
+private:
+    float m_x{ 0.0 };
+    float m_y{ 0.0 };
+    float m_z{ 0.0 };
+    float m_w{ 1.0 };
+};
