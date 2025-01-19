6041a262a8909dbf00700a1ef64e3c968a5e959b
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-19 15:13:08 -0600

    Added tests for homogeneous coordinate constructor and assignment operator

--- src/gfx/point.test.cpp
+++ src/gfx/point.test.cpp
@@ -6,10 +6,10 @@ TEST(GraphicsPoint, DefaultConstructor)
 {
     const gfx::Point pt;
 
-    EXPECT_FLOAT_EQ(pt.x(), 0.0);
-    EXPECT_FLOAT_EQ(pt.y(), 0.0);
-    EXPECT_FLOAT_EQ(pt.z(), 0.0);
-    EXPECT_FLOAT_EQ(pt.w(), 1.0);
+    ASSERT_FLOAT_EQ(pt.x(), 0.0);
+    ASSERT_FLOAT_EQ(pt.y(), 0.0);
+    ASSERT_FLOAT_EQ(pt.z(), 0.0);
+    ASSERT_FLOAT_EQ(pt.w(), 1.0);
 }
 
 // Tests the standard constructor
@@ -17,24 +17,52 @@ TEST(GraphicsPoint, Constructor)
 {
     const gfx::Point pt{ 4.0, -4.0, 3.0 };
 
-    EXPECT_FLOAT_EQ(pt.x(), 4.0);
-    EXPECT_FLOAT_EQ(pt.y(), -4.0);
-    EXPECT_FLOAT_EQ(pt.z(), 3.0);
-    EXPECT_FLOAT_EQ(pt.w(), 1.0);
+    ASSERT_FLOAT_EQ(pt.x(), 4.0);
+    ASSERT_FLOAT_EQ(pt.y(), -4.0);
+    ASSERT_FLOAT_EQ(pt.z(), 3.0);
+    ASSERT_FLOAT_EQ(pt.w(), 1.0);
 }
 
+// Tests the homogeneous coordinate constructor
+TEST(GraphicsPoint, HomogeneousConstructor)
+{
+    const gfx::Point pt{ 4.0, -4.0, 3.0, 0.5 };
+
+    ASSERT_FLOAT_EQ(pt.x(), 4.0);
+    ASSERT_FLOAT_EQ(pt.y(), -4.0);
+    ASSERT_FLOAT_EQ(pt.z(), 3.0);
+    ASSERT_FLOAT_EQ(pt.w(), 0.5);
+}
+
+
+// Tests the copy constructor
 TEST(GraphicsPoint, CopyConstructor)
 {
     const gfx::Point pt_src{ 4.0, -4.0, 3.0 };
     const gfx::Point pt_cpy{ pt_src };
 
-    EXPECT_FLOAT_EQ(pt_cpy.x(), 4.0);
-    EXPECT_FLOAT_EQ(pt_cpy.y(), -4.0);
-    EXPECT_FLOAT_EQ(pt_cpy.z(), 3.0);
-    EXPECT_FLOAT_EQ(pt_cpy.w(), 1.0);
+    ASSERT_FLOAT_EQ(pt_cpy.x(), 4.0);
+    ASSERT_FLOAT_EQ(pt_cpy.y(), -4.0);
+    ASSERT_FLOAT_EQ(pt_cpy.z(), 3.0);
+    ASSERT_FLOAT_EQ(pt_cpy.w(), 1.0);
 }
 
-TEST(GraphicsPoint, ComparisonOperator)
+// Tests the assignment operator
+TEST(GraphicsVector, AssignmentOperator)
+{
+    gfx::Point pt_a{ 1.0, 2.0, 3.0 };
+    const gfx::Point pt_b{ 17.0, -3.5, 1.2 };
+
+    pt_a = pt_b;
+
+    ASSERT_FLOAT_EQ(pt_a.x(), 17.0);
+    ASSERT_FLOAT_EQ(pt_a.y(), -3.5);
+    ASSERT_FLOAT_EQ(pt_a.z(), 1.2);
+    ASSERT_FLOAT_EQ(pt_a.w(), 1.0);
+}
+
+// Tests the equality operator
+TEST(GraphicsPoint, EqualityOperator)
 {
     const gfx::Point pt_a{ 4.0, -4.0, 3.0 };
     const gfx::Point pt_b{ 4.0, -4.0, 3.0 };
@@ -42,6 +70,7 @@ TEST(GraphicsPoint, ComparisonOperator)
     EXPECT_TRUE(pt_a == pt_b);
 }
 
+// Tests the inequality operator
 TEST(GraphicsPoint, InequalityOperator)
 {
     const gfx::Point pt_a{ 4.0, -4.0, 3.0 };
