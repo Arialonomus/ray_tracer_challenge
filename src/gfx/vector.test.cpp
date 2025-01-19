44116eb2563971006437f3ae324bec179957ce74
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-19 12:05:05 -0600

    Added tests for the assignment and vector-vector addition operators

--- src/gfx/vector.test.cpp
+++ src/gfx/vector.test.cpp
@@ -23,6 +23,7 @@ TEST(GraphicsVector, Constructor)
     EXPECT_FLOAT_EQ(vec.w(), 0.0);
 }
 
+// Tests the copy constructor
 TEST(GraphicsVector, CopyConstructor)
 {
     const gfx::Vector vec_src{ 4.0, -4.0, 3.0 };
@@ -34,6 +35,21 @@ TEST(GraphicsVector, CopyConstructor)
     EXPECT_FLOAT_EQ(vec_cpy.w(), 0.0);
 }
 
+// Tests the assignment operator
+TEST(GraphicsVector, AssignmentOperator)
+{
+    gfx::Vector vec_a{ 1.0, 2.0, 3.0 };
+    const gfx::Vector vec_b{ 17.0, -3.5, 1.2 };
+
+    vec_a = vec_b;
+
+    EXPECT_FLOAT_EQ(vec_a.x(), 17.0);
+    EXPECT_FLOAT_EQ(vec_a.y(), -3.5);
+    EXPECT_FLOAT_EQ(vec_a.z(), 1.2);
+    EXPECT_FLOAT_EQ(vec_a.w(), 0.0);
+}
+
+// Tests the comparison operator
 TEST(GraphicsVector, ComparisonOperator)
 {
     const gfx::Vector vec_a{ 4.0, -4.0, 3.0 };
@@ -42,10 +58,25 @@ TEST(GraphicsVector, ComparisonOperator)
     EXPECT_TRUE(vec_a == vec_b);
 }
 
+// Tests the inequality operator
 TEST(GraphicsVector, InequalityOperator)
 {
     const gfx::Vector vec_a{ 4.0, -4.0, 3.0 };
     const gfx::Vector vec_b{ -1.0, -5.0, 0.0 };
 
     EXPECT_TRUE(vec_a != vec_b);
+}
+
+// Tests vector-vector addition with the addition operator
+TEST(GraphicsVector, VectorVectorAddition)
+{
+    const gfx::Vector vec_a{ 1.0, 2.0, -3.0 };
+    const gfx::Vector vec_b{ 5.0, -4.5, 7.1 };
+
+    const gfx::Vector vec_c = vec_a + vec_b;
+
+    EXPECT_FLOAT_EQ(vec_c.x(), 6.0);
+    EXPECT_FLOAT_EQ(vec_c.y(), -2.5);
+    EXPECT_FLOAT_EQ(vec_c.z(), 4.1);
+    EXPECT_FLOAT_EQ(vec_c.w(), 0.0);
 }
