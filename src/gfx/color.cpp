b3dd79c94d0012099a80bc6b0297db101e446a62
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-20 10:43:04 -0600

    Added color-color multiplication (Hadamard product) using multiplication and multiplication shorthand operators

--- src/gfx/color.cpp
+++ src/gfx/color.cpp
@@ -20,7 +20,17 @@ gfx::Color& gfx::Color::operator-=(const Color& rhs)
     return *this;
 }
 
-// Color Scalar Multiplication Shorthand Operator
+// Color Multiplication (Hadamard Product) Shorthand Operator
+gfx::Color& gfx::Color::operator*=(const Color& rhs)
+{
+    m_r *= rhs.r();
+    m_g *= rhs.g();
+    m_b *= rhs.b();
+
+    return *this;
+}
+
+// Scalar Multiplication Shorthand Operator
 gfx::Color& gfx::Color::operator*=(const float scalar)
 {
     m_r *= scalar;
@@ -42,13 +52,19 @@ gfx::Color gfx::operator-(const Color& lhs, const Color& rhs)
     return Color{ lhs.r() - rhs.r(), lhs.g() - rhs.g(), lhs.b() - rhs.b() };
 }
 
-// Color Scalar Multiplication Operator (Color Left-Hand Operand)
+// Color Multiplication (Hadamard Product) Operator
+gfx::Color gfx::operator*(const Color& lhs, const Color& rhs)
+{
+    return Color{ lhs.r() * rhs.r(), lhs.g() * rhs.g(), lhs.b() * rhs.b() };
+}
+
+// Scalar Multiplication Operator (Color Left-Hand Operand)
 gfx::Color gfx::operator*(const Color& lhs, const float rhs)
 {
     return Color{ lhs.r() * rhs, lhs.g() * rhs, lhs.b() * rhs };
 }
 
-// Color Scalar Multiplication Operator (Color Right-Hand Operand)
+// Scalar Multiplication Operator (Color Right-Hand Operand)
 gfx::Color gfx::operator*(const float lhs, const Color& rhs)
 {
     return Color{ rhs.r() * lhs, rhs.g() * lhs, rhs.b() * lhs };
