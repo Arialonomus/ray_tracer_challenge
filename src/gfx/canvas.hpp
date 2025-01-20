c37c7d92da3a6c2ac8e806776a3c3d4da666e720
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-20 15:31:16 -0600

    Added acessor methods

--- src/gfx/canvas.hpp
+++ src/gfx/canvas.hpp
@@ -15,6 +15,12 @@ namespace gfx
         Canvas() = delete;
         Canvas(const size_t width, const size_t height) : m_pixels(width* height), m_grid(m_pixels.data(), height, width) {}
 
+        /* Accessors */
+
+        [[nodiscard]] size_t width() const { return m_grid.extents().extent(1); }
+        [[nodiscard]] size_t height() const { return m_grid.extents().extent(0); }
+        [[nodiscard]] Color operator[](const size_t col, const size_t row) const { return m_grid[col, row]; }
+
     private:
         std::vector<Color> m_pixels;
         std::mdspan<Color, std::extents<size_t, std::dynamic_extent, std::dynamic_extent>, std::layout_left> m_grid;
