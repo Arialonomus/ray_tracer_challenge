0c3254559b45a318a44fddd785068b1fe30a2390
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-19 18:12:09 -0600

    Added definition of color class with constructor, acessors, and big five

---
+++ src/gfx/color.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace gfx
+{
+    class Color
+    {
+    public:
+        /* Constructors */
+
+        Color() = default;
+        Color(const float r, const float g, const float b) : m_r{ r }, m_g{ g }, m_b{ b } {}
+        Color(const Color&) = default;
+        Color(Color&&) = default;
+
+        /* Destructor */
+
+        ~Color() = default;
+
+        /* Assignment Operators */
+
+        Color& operator=(const Color&) = default;
+        Color& operator=(Color&&) = default;
+
+        /* Accessors */
+
+        [[nodiscard]] float r() const { return m_r; }
+        [[nodiscard]] float g() const { return m_g; }
+        [[nodiscard]] float b() const { return m_b; }
+
+    private:
+        float m_r{ 0.0 };
+        float m_g{ 0.0 };
+        float m_b{ 0.0 };
+    };
+}
