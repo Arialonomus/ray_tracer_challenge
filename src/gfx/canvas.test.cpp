9199170afc85d224bc0b8524cc84cb18601db578
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-21 13:16:17 -0600

    Added test for exporting a small canvas to PPM file format

--- src/gfx/canvas.test.cpp
+++ src/gfx/canvas.test.cpp
@@ -3,7 +3,8 @@
 #include "canvas.hpp"
 #include "color.hpp"
 
-#include <print>
+#include <string>
+#include <sstream>
 
 // Tests the standard constructor
 TEST(GraphicsCanvas, Constructor)
@@ -35,4 +36,57 @@ TEST(GraphicsCanvas, WritePixelColor)
     canvas[2, 3] = red;
     const gfx::Color test_pixel = canvas[2, 3];
     ASSERT_TRUE(test_pixel == red);
+}
+
+// Tests exporting a small canvas (no line wrapping) to the PPM format
+TEST(GraphicsCanvas, ExportPPMSmallCanvas)
+{
+    constexpr size_t width = 5;
+    constexpr size_t height = 3;
+    const gfx::Canvas canvas{ width, height };
+
+    // Set the test colors
+    canvas[0, 0] = gfx::Color{ 1.5, 0, 0 };
+    canvas[2, 1] = gfx::Color{ 0, 0.5, 0 };
+    canvas[4, 2] = gfx::Color{ -0.5, 0, 1 };
+
+    const std::string ppm_string = gfx::exportAsPPM(canvas);
+    std::istringstream ppm_stream{ ppm_string };
+
+    // Test that the header is correctly output
+    constexpr std::string_view exp_header_identifier = "P3";
+    constexpr std::string_view exp_header_dimensions = "5 3";
+    constexpr std::string_view exp_header_color_max = "255";
+
+    std::string act_header_identifier {};
+    std::getline(ppm_stream, act_header_identifier);
+    EXPECT_TRUE(act_header_identifier == exp_header_identifier);
+
+    std::string act_header_dimensions {};
+    std::getline(ppm_stream, act_header_dimensions);
+    EXPECT_TRUE(act_header_dimensions == exp_header_dimensions);
+
+    std::string act_header_color_max {};
+    std::getline(ppm_stream, act_header_color_max);
+    EXPECT_TRUE(act_header_color_max == exp_header_color_max);
+
+    // Test the color data is correctly output
+    constexpr std::string_view exp_row_4 = "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
+    constexpr std::string_view exp_row_5 = "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0";
+    constexpr std::string_view exp_row_6 = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255";
+
+    std::string act_row_4 {};
+    std::getline(ppm_stream, act_row_4);
+    EXPECT_TRUE(act_row_4 == exp_row_4);
+
+    std::string act_row_5 {};
+    std::getline(ppm_stream, act_row_5);
+    EXPECT_TRUE(act_row_5 == exp_row_5);
+
+    std::string act_row_6 {};
+    std::getline(ppm_stream, act_row_6);
+    EXPECT_TRUE(act_row_6 == exp_row_6);
+
+    // Ensure that file ends in a newline
+    EXPECT_TRUE(ppm_string.at(ppm_string.length() - 1) == '\n');
 }
