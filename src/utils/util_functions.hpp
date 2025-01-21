779ec7394ae75bc1590a3661ba5470ec484b6ba1
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-21 13:27:45 -0600

    Added template function for clamped scaling

--- src/utils/util_functions.hpp
+++ src/utils/util_functions.hpp
@@ -11,5 +11,19 @@ namespace utils
     {
         return std::fabs(f1 - f2) <= std::numeric_limits<T>::epsilon() * std::fmax(std::fabs(f1), std::fabs(f2));
     }
-    
+
+    template<typename I, typename O>
+    O clampedScale(I input, O min, O max)
+    {
+        O scaled_value = std::round(input * max);
+        if (scaled_value <= min) {
+            return min;
+        }
+        else if (scaled_value >= max) {
+            return max;
+        }
+        else {
+            return scaled_value;
+        }
+    }
 }
