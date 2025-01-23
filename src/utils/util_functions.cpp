bec935f78f45200bf72fc2a094d74b69dc361897
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-23 17:05:41 -0600

    Refactored areEqual() to handle cases where the result might be near zero

--- src/utils/util_functions.cpp
+++ src/utils/util_functions.cpp
@@ -2,5 +2,9 @@
 
 bool utils::areEqual(float f1, float f2)
 {
-    return std::fabs(f1 - f2) <= utils::EPSILON * std::fmax(std::fabs(f1), std::fabs(f2));
+    float difference = std::fabs(f1 - f2);
+    if (difference <= utils::EPSILON) {
+        return true;
+    }
+    return difference <= utils::EPSILON * std::fmax(std::fabs(f1), std::fabs(f2));
 }
