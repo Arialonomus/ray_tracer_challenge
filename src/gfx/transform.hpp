2782c385f9c05c2ee879122b011b0fe2afd02788
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-23 17:41:15 -0600

    Added factory functions for generating translation matrices

---
+++ src/gfx/transform.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "matrix4.hpp"
+#include "vector4.hpp"
+
+namespace gfx {
+    /* Transformation Matrix Factory Functions */
+
+    // Returns a matrix representing a translation along the vector formed by passed-in coordinates
+    [[nodiscard]] Matrix4 getTranslationMatrix(const float x, const float y, const float z)
+    {
+        return Matrix4{
+                1.0, 0.0, 0.0, x,
+                0.0, 1.0, 0.0, y,
+                0.0, 0.0, 1.0, z,
+                0.0, 0.0, 0.0, 1.0
+        };
+    }
+
+    // Returns a matrix representing a translation along a passed-in vector
+    [[nodiscard]] Matrix4 getTranslationMatrix(const Vector4& vec)
+    {
+        return Matrix4{
+                1.0, 0.0, 0.0, vec.x(),
+                0.0, 1.0, 0.0, vec.y(),
+                0.0, 0.0, 1.0, vec.z(),
+                0.0, 0.0, 0.0, 1.0
+        };
+    }
+}
