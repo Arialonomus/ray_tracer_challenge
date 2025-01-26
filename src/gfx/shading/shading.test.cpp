31af067e87d60ba5ed120328a17881bbacbd2cbb
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-25 18:34:22 -0600

    Added tests for createSurfaceColor() with various light/viewpoint configurations

---
+++ src/gfx/shading/shading.test.cpp
@@ -0,0 +1,111 @@
+#include "gtest/gtest.h"
+#include "shading_functions.hpp"
+
+#include <cmath>
+
+#include "color.hpp"
+#include "material.hpp"
+#include "light.hpp"
+#include "vector4.hpp"
+
+// Tests calculating surface color with the view origin between the light and the surface
+TEST(GraphicsShading, ViewBetweenLightAndSurface)
+{
+    const gfx::Material material{ };
+    const gfx::PointLight point_light{ gfx::Color{ 1, 1, 1 },
+                                       gfx::createPoint(0, 0, -10) };
+    const gfx::Vector4 surface_position{ 0, 0, 0, 1 };
+    const gfx::Vector4 surface_normal{ 0, 0, -1, 0 };
+    const gfx::Vector4 view_vector{ 0, 0, -1, 0 };
+
+    const gfx::Color color_expected{ 1.9, 1.9, 1.9 };
+    const gfx::Color color_actual{ gfx::calculateSurfaceColor(material,
+                                                              point_light,
+                                                              surface_position,
+                                                              surface_normal,
+                                                              view_vector) };
+
+    EXPECT_EQ(color_actual, color_expected);
+}
+
+// Tests calculating surface color with the view origin between the light
+// and the surface and the light offset 45 degrees
+TEST(GraphicsShading, ViewBetweenLightAndSurfaceViewOffset45Degrees)
+{
+    const gfx::Material material{ };
+    const gfx::PointLight point_light{ gfx::Color{ 1, 1, 1 },
+                                       gfx::createPoint(0, 0, -10) };
+    const gfx::Vector4 surface_position{ 0, 0, 0, 1 };
+    const gfx::Vector4 surface_normal{ 0, 0, -1, 0 };
+    const gfx::Vector4 view_vector{  0, M_SQRT2f / 2, -M_SQRT2f / 2, 0 };
+
+    const gfx::Color color_expected{ 1.0, 1.0, 1.0 };
+    const gfx::Color color_actual{ gfx::calculateSurfaceColor(material,
+                                                              point_light,
+                                                              surface_position,
+                                                              surface_normal,
+                                                              view_vector) };
+
+    EXPECT_EQ(color_actual, color_expected);
+}
+
+// Tests calculating surface color with the view origin opposite the surface
+// and the light offset 45 degrees
+TEST(GraphicsShading, ViewOppositeSurfaceLightOffset45Degrees)
+{
+    const gfx::Material material{ };
+    const gfx::PointLight point_light{ gfx::Color{ 1, 1, 1 },
+                                       gfx::createPoint(0, 10, -10) };
+    const gfx::Vector4 surface_position{ 0, 0, 0, 1 };
+    const gfx::Vector4 surface_normal{ 0, 0, -1, 0 };
+    const gfx::Vector4 view_vector{ 0, 0, -1, 0 };
+
+    const gfx::Color color_expected{ 0.76364, 0.7364, 0.7364 };
+    const gfx::Color color_actual{ gfx::calculateSurfaceColor(material,
+                                                              point_light,
+                                                              surface_position,
+                                                              surface_normal,
+                                                              view_vector) };
+
+    EXPECT_EQ(color_actual, color_expected);
+}
+
+// Tests calculating surface color with the view origin in the path of the light reflection vector
+TEST(GraphicsShading, ViewInPathOfReflectionVector)
+{
+    const gfx::Material material{ };
+    const gfx::PointLight point_light{ gfx::Color{ 1, 1, 1 },
+                                       gfx::createPoint(0, 10, -10) };
+    const gfx::Vector4 surface_position{ 0, 0, 0, 1 };
+    const gfx::Vector4 surface_normal{ 0, 0, -1, 0 };
+    const gfx::Vector4 view_vector{ 0, -M_SQRT2f / 2, -M_SQRT2f / 2, 0 };
+
+    const gfx::Color color_expected{ 1.66364, 1.6364, 1.6364 };
+    const gfx::Color color_actual{ gfx::calculateSurfaceColor(material,
+                                                              point_light,
+                                                              surface_position,
+                                                              surface_normal,
+                                                              view_vector) };
+
+    EXPECT_EQ(color_actual, color_expected);
+}
+
+// Tests calculating surface color with the surface between the view and the light
+TEST(GraphicsShading, SurfaceBetweenViewAndLight)
+{
+    const gfx::Material material{ };
+    const gfx::PointLight point_light{ gfx::Color{ 1, 1, 1 },
+                                       gfx::createPoint(0, 0, 10) };
+    const gfx::Vector4 surface_position{ 0, 0, 0, 1 };
+    const gfx::Vector4 surface_normal{ 0, 0, -1, 0 };
+    const gfx::Vector4 view_vector{ 0, 0, -1, 0 };
+
+    const gfx::Color color_expected{ 0.1, 0.1, 0.1 };
+    const gfx::Color color_actual{ gfx::calculateSurfaceColor(material,
+                                                              point_light,
+                                                              surface_position,
+                                                              surface_normal,
+                                                              view_vector) };
+
+    EXPECT_EQ(color_actual, color_expected);
+}
