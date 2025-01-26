4ef204aea2c5e669de722581ee95db532e7439da
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-26 15:50:44 -0600

    Added tests for constructing a detailed intersection object

--- src/gfx/geometry/intersection.test.cpp
+++ src/gfx/geometry/intersection.test.cpp
@@ -59,6 +59,82 @@ TEST(GraphicsIntersection, InequalityOperator)
     ASSERT_TRUE(intersection_a != intersection_b);
 }
 
+// Tests the standard constructor for detailed intersections (when the intersection is outside the object)
+TEST(GraphicsIntersection, DetailedStandardConstructorOutside)
+{
+    const gfx::Sphere sphere{ };
+    const gfx::Intersection base_intersection{ 4, sphere };
+    const gfx::Ray ray{ 0, 0, -5,
+                        0, 0, 1 };
+    const gfx::DetailedIntersection detailed_intersection{ base_intersection, ray };
+
+    ASSERT_FLOAT_EQ(detailed_intersection.getT(), 4);
+    ASSERT_EQ(&detailed_intersection.getObject(), &sphere);
+    ASSERT_EQ(detailed_intersection.getSurfacePosition(), gfx::createPoint(0, 0, -1));
+    ASSERT_EQ(detailed_intersection.getSurfaceNormal(), gfx::createVector(0, 0, -1));
+    ASSERT_EQ(detailed_intersection.getViewVector(), gfx::createVector(0, 0, -1));
+    ASSERT_FALSE(detailed_intersection.isInsideObject());
+}
+
+// Tests the standard constructor for detailed intersections (when the intersection is inside the object)
+TEST(GraphicsIntersection, DetailedStandardConstructorInside)
+{
+    const gfx::Sphere sphere{ };
+    const gfx::Intersection base_intersection{ 1, sphere };
+    const gfx::Ray ray{ 0, 0, 0,
+                        0, 0, 1 };
+    const gfx::DetailedIntersection detailed_intersection{ base_intersection, ray };
+
+    ASSERT_FLOAT_EQ(detailed_intersection.getT(), 1);
+    ASSERT_EQ(&detailed_intersection.getObject(), &sphere);
+    ASSERT_EQ(detailed_intersection.getSurfacePosition(), gfx::createPoint(0, 0, 1));
+    ASSERT_EQ(detailed_intersection.getSurfaceNormal(), gfx::createVector(0, 0, -1));
+    ASSERT_EQ(detailed_intersection.getViewVector(), gfx::createVector(0, 0, -1));
+    ASSERT_TRUE(detailed_intersection.isInsideObject());
+}
+
+// Tests the copy constructor for detailed intersections
+TEST(GraphicsIntersection, DetailedCopyConstructor)
+{
+    const gfx::Sphere sphere{ };
+    const gfx::Intersection base_intersection{ 4, sphere };
+    const gfx::Ray ray{ 0, 0, -5,
+                        0, 0, 1 };
+    const gfx::DetailedIntersection detailed_intersection_src{ base_intersection, ray };
+    const gfx::DetailedIntersection detailed_intersection_cpy{ detailed_intersection_src };
+
+    ASSERT_FLOAT_EQ(detailed_intersection_cpy.getT(), 4);
+    ASSERT_EQ(&detailed_intersection_cpy.getObject(), &sphere);
+    ASSERT_EQ(detailed_intersection_cpy.getSurfacePosition(), gfx::createPoint(0, 0, -1));
+    ASSERT_EQ(detailed_intersection_cpy.getSurfaceNormal(), gfx::createVector(0, 0, -1));
+    ASSERT_EQ(detailed_intersection_cpy.getViewVector(), gfx::createVector(0, 0, -1));
+    ASSERT_FALSE(detailed_intersection_cpy.isInsideObject());
+}
+
+// Tests the assignment operator for detailed intersections
+TEST(GraphicsIntersection, DetailedAssignmentOperator)
+{
+    const gfx::Sphere sphere_a{ };
+    const gfx::Intersection base_intersection_a{ 4, sphere_a };
+    const gfx::Ray ray_a{ 0, 0, -5,
+                        0, 0, 1 };
+    const gfx::DetailedIntersection detailed_intersection_a{ base_intersection_a, ray_a };
+    const gfx::Sphere sphere_b{ };
+    const gfx::Intersection base_intersection_b{ 1, sphere_b };
+    const gfx::Ray ray_b{ 0, 0, -10,
+                          1, 1, 1 };
+    gfx::DetailedIntersection detailed_intersection_b{ base_intersection_b, ray_b };
+
+    detailed_intersection_b = detailed_intersection_a;
+
+    ASSERT_FLOAT_EQ(detailed_intersection_b.getT(), 4);
+    ASSERT_EQ(&detailed_intersection_b.getObject(), &sphere_a);
+    ASSERT_EQ(detailed_intersection_b.getSurfacePosition(), gfx::createPoint(0, 0, -1));
+    ASSERT_EQ(detailed_intersection_b.getSurfaceNormal(), gfx::createVector(0, 0, -1));
+    ASSERT_EQ(detailed_intersection_b.getViewVector(), gfx::createVector(0, 0, -1));
+    ASSERT_FALSE(detailed_intersection_b.isInsideObject());
+}
+
 // Tests the getHit functionality for 2-intersection groups when all intersections have positive t values
 TEST(GraphicsIntersection, GetHitAllPositiveT)
 {
