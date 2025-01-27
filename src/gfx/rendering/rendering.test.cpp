c25e31ad6fe66929b9aea1ed683c1c40bcee658d
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-27 13:06:20 -0600

    Added test of render() function

--- src/gfx/rendering/rendering.test.cpp
+++ src/gfx/rendering/rendering.test.cpp
@@ -7,6 +7,7 @@
 #include "material.hpp"
 #include "light.hpp"
 #include "vector4.hpp"
+#include "transform.hpp"
 
 // Tests calculating surface color with the view origin between the light and the surface
 TEST(GraphicsRendering, ViewBetweenLightAndSurface)
@@ -108,4 +109,34 @@ TEST(GraphicsRendering, SurfaceBetweenViewAndLight)
                                                               view_vector) };
 
     EXPECT_EQ(color_actual, color_expected);
+}
+
+// Tests rendering a world to a canvas
+TEST(GraphicsRendering, RenderWorld)
+{
+    gfx::Material material{ };
+    material.setColor(0.8, 1.0, 0.6);
+    material.setDiffuse(0.7);
+    material.setSpecular(0.2);
+
+    gfx::Sphere sphere_a{ material };
+    gfx::Sphere sphere_b{ gfx::createScalingMatrix(0.5) };
+    const std::vector<std::reference_wrapper<gfx::Sphere>> object_list{ sphere_a, sphere_b };
+    const gfx::World world{ object_list };
+
+    const gfx::Vector4 from_position{ gfx::createPoint(0, 0, -5) };
+    const gfx::Vector4 to_position{ gfx::createPoint(0, 0, 0) };
+    const gfx::Vector4 up_vector{ gfx::createVector(0, 1, 0) };
+    const gfx::Matrix4 view_transform_matrix{
+            gfx::createViewTransformMatrix(
+                    from_position,
+                    to_position,
+                    up_vector) };
+    const gfx::Camera camera{ 11, 11, M_PI_2f,view_transform_matrix };
+
+    const gfx::Canvas image{ gfx::render(world, camera) };
+
+    const gfx::Color color_center_pixel_expected{ 0.380661, 0.475827, 0.285496 };
+    const gfx::Color color_center_pixel_actual{ image[5, 5] };
+    EXPECT_EQ(color_center_pixel_actual, color_center_pixel_expected);
 }
