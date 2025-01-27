14dd5b8136c5a1ae7cdfe474a5e0b81528a6b421
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-27 09:20:15 -0600

    Refactored Material struct into fully-fledged class with constructors, accessors, mutators, equality operator, and Big Five

---
+++ src/gfx/shading/material.cpp
@@ -0,0 +1,16 @@
+#include "material.hpp"
+
+#include "util_functions.hpp"
+
+namespace gfx {
+    // Equality Operator
+    bool gfx::Material::operator==(const Material& rhs) const
+    {
+        return
+            m_color == rhs.getColor() &&
+            utils::areEqual(m_ambient, rhs.getAmbient()) &&
+            utils::areEqual(m_diffuse, rhs.getDiffuse()) &&
+            utils::areEqual(m_specular, rhs.getSpecular()) &&
+            utils::areEqual(m_shininess, rhs.getShininess());
+    }
+}
--- src/gfx/shading/material.hpp
+++ src/gfx/shading/material.hpp
@@ -3,11 +3,88 @@
 #include "color.hpp"
 
 namespace gfx {
-    struct Material {
-        Color color{ 1, 1, 1 };
-        float ambient{ 0.1 };
-        float diffuse{ 0.9 };
-        float specular{ 0.9 };
-        float shininess{ 200 };
+    class Material {
+    public:
+        Material() = default;
+        Material(const Color& color,
+                 const float ambient,
+                 const float diffuse,
+                 const float specular,
+                 const float shininess)
+                : m_color{ color },
+                m_ambient{ ambient },
+                m_diffuse{ diffuse },
+                m_specular{ specular },
+                m_shininess{ shininess }
+        {}
+        Material(const float color_r,
+                 const float color_g,
+                 const float color_b,
+                 const float ambient,
+                 const float diffuse,
+                 const float specular,
+                 const float shininess)
+                : m_color{ Color{ color_r, color_g, color_b } },
+                  m_ambient{ ambient },
+                  m_diffuse{ diffuse },
+                  m_specular{ specular },
+                  m_shininess{ shininess }
+        {}
+        Material(const Color& color)
+                : m_color{ color },
+                  m_ambient{ 0.1 },
+                  m_diffuse{ 0.9 },
+                  m_specular{ 0.9 },
+                  m_shininess{ 200 }
+        {}
+        Material(const float ambient,
+                 const float diffuse,
+                 const float specular,
+                 const float shininess)
+                : m_color{ 1, 1, 1 },
+                  m_ambient{ ambient },
+                  m_diffuse{ diffuse },
+                  m_specular{ specular },
+                  m_shininess{ shininess }
+        {}
+        Material(const Material&) = default;
+        Material(Material&&) = default;
+
+        /* Destructor */
+
+        virtual ~Material() = default;
+
+        /* Assignment Operators */
+
+        Material& operator=(const Material&) = default;
+        Material& operator=(Material&&) = default;
+
+        /* Accessors */
+
+        [[nodiscard]] const Color& getColor() const { return m_color; }
+        [[nodiscard]] float getAmbient() const { return m_ambient; }
+        [[nodiscard]] float getDiffuse() const { return m_diffuse; }
+        [[nodiscard]] float getSpecular() const { return m_specular; }
+        [[nodiscard]] float getShininess() const { return m_shininess; }
+
+        /* Mutators */
+
+        const void setColor(const Color& color) { m_color = color; }
+        const void setColor(const float r, const float g, const float b) { m_color.setValues(r, g, b); }
+        const void setAmbient(const float ambient) { m_ambient = ambient; }
+        const void setDiffuse(const float diffuse) { m_diffuse = diffuse; }
+        const void setSpecular(const float specular) { m_specular = specular; }
+        const void setShininess(const float shininess) { m_shininess = shininess; }
+
+        /* Comparison Operator Overloads */
+
+        [[nodiscard]] bool operator==(const Material& rhs) const;
+
+    private:
+        Color m_color{ 1, 1, 1 };
+        float m_ambient{ 0.1 };
+        float m_diffuse{ 0.9 };
+        float m_specular{ 0.9 };
+        float m_shininess{ 200 };
     };
 }
