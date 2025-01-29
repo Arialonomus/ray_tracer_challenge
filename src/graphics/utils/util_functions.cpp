cddc8ef9881ac62be6e08e8475b9c27fcde9c9c3
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-29 14:55:06 -0600

    Reverted areEqual to a template function and added additional relative comparison functions for floating point numbers

--- src/graphics/utils/util_functions.cpp
+++ src/graphics/utils/util_functions.cpp
@@ -1,12 +1,1 @@
 #include "util_functions.hpp"
-
-namespace utils {
-    bool areEqual(double f1, double f2)
-    {
-        double difference = std::abs(f1 - f2);
-        if (difference <= EPSILON) {
-            return true;
-        }
-        return difference <= EPSILON * std::fmax(std::abs(f1), std::abs(f2));
-    }
-}
--- src/graphics/utils/util_functions.hpp
+++ src/graphics/utils/util_functions.hpp
@@ -1,13 +1,53 @@
 #pragma once
 
 #include <cmath>
+#include <limits>
 
 namespace utils
 {
-    constexpr double EPSILON{ 1e-6 };    // Tolerance value for relative double comparison
+    constexpr double EPSILON{ 1e-6 };
+
+    /* Relative Comparison Functions */
 
     // Uses relative comparison for two floating point numbers to determine if they are equal
-    bool areEqual(double f1, double f2);
+    template<typename T>
+    bool areEqual(T f1, T f2)
+    {
+        const T epsilon{ EPSILON };
+        const T difference = std::abs(f1 - f2);
+        if (difference <= epsilon) {
+            return true;
+        }
+        return difference <= epsilon * std::fmax(std::abs(f1), std::abs(f2));
+    }
+
+    // Uses relative comparison for floating-point numbers to determine f1 is less than f2
+    template<typename T>
+    bool isLess(T f1, T f2)
+    {
+        return !areEqual(f1, f2) && (f1 < f2);
+    }
+
+    // Uses relative comparison for floating-point numbers to determine f1 is less than f2
+    template<typename T>
+    bool isLessOrEqual(T f1, T f2)
+    {
+        return areEqual(f1, f2) || (f1 < f2);
+    }
+
+    // Uses relative comparison for floating-point numbers to determine f1 is greater than f2
+    template<typename T>
+    bool isGreater(T f1, T f2)
+    {
+        return !areEqual(f1, f2) && (f1 > f2);
+    }
+
+    // Uses relative comparison for floating-point numbers to determine f1 is greater than f2
+    template<typename T>
+    bool isGreaterOrEqual(T f1, T f2)
+    {
+        return areEqual(f1, f2) || (f1 > f2);
+    }
 
     // Scales a value relative to some maximum, clamping any values higher or lower than the bounds
     template<typename I, typename O>
