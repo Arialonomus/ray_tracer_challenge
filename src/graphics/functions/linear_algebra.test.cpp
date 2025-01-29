4f1a1da337dc60060af18ad442a2f9980a736fc2
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-29 10:44:26 -0600

    Added tests for finding determinant of square matrices of dimension 2 through 4

--- src/graphics/functions/linear_algebra.test.cpp
+++ src/graphics/functions/linear_algebra.test.cpp
@@ -38,3 +38,67 @@ TEST(GraphicsLinearAlgebra, Submatrix3x3)
 
     ASSERT_EQ(submatrix_vals_actual, submatrix_vals_expected);
 }
+
+// Tests calculating the determinant of a flattened 2x2 matrix
+TEST(GraphicsLinearAlgebra, Determinant2x2)
+{
+    const std::vector<float> matrix_a_vals{
+            1.0, 5.0,
+            -3.0, 2.0,
+    };
+    const float matrix_a_determinant_actual{ gfx::calculateDeterminant(matrix_a_vals) };
+    const float matrix_a_determinant_expected{ 17 };
+
+    EXPECT_FLOAT_EQ(matrix_a_determinant_actual, matrix_a_determinant_expected);
+}
+
+// Tests calculating the determinant of a flattened 3x3 matrix
+TEST(GraphicsLinearAlgebra, Determinant3x3)
+{
+    const std::vector<float> matrix_a_vals{
+            1.0, 2.0, 6.0,
+            -5.0, 8.0, -4.0,
+            2.0, 6.0, 4.0
+    };
+    const float matrix_a_determinant_actual{ gfx::calculateDeterminant(matrix_a_vals) };
+    const float matrix_a_determinant_expected{ -196 };
+
+    EXPECT_FLOAT_EQ(matrix_a_determinant_actual, matrix_a_determinant_expected);
+}
+
+// Tests calculating the determinant of a flattened 4x4 matrix
+TEST(GraphicsLinearAlgebra, Determinant4x4)
+{
+    const std::vector<float> matrix_a_vals{
+            -2.0, -8.0, 3.0, 5.0,
+            -3.0, 1.0, 7.0, 3.0,
+            1.0, 2.0, -9.0, 6.0,
+            -6.0, 7.0, 7.0, -9.0
+    };
+    const float matrix_a_determinant_actual{ gfx::calculateDeterminant(matrix_a_vals) };
+    const float matrix_a_determinant_expected{ -4071 };
+
+    EXPECT_FLOAT_EQ(matrix_a_determinant_actual, matrix_a_determinant_expected);
+
+    const std::vector<float> matrix_b_vals{
+            6.0, 4.0, 4.0, 4.0,
+            5.0, 5.0, 7.0, 6.0,
+            4.0, -9.0, 3.0, -7.0,
+            9.0, 1.0, 7.0, -6.0
+    };
+    const float matrix_b_determinant_actual{ gfx::calculateDeterminant(matrix_b_vals) };
+    const float matrix_b_determinant_expected{ -2120 };
+
+    EXPECT_FLOAT_EQ(matrix_b_determinant_actual, matrix_b_determinant_expected);
+
+    const std::vector<float> matrix_c_vals{
+            -4.0, 2.0, -2.0, -3.0,
+            9.0, 6.0, 2.0, 6.0,
+            0.0, -5.0, 1.0, -5.0,
+            0.0, 0.0, 0.0, 0.0
+    };
+    const float matrix_c_determinant_actual{ gfx::calculateDeterminant(matrix_c_vals) };
+    const float matrix_c_determinant_expected{ 0 };
+
+    EXPECT_FLOAT_EQ(matrix_c_determinant_actual, matrix_c_determinant_expected);
+}
