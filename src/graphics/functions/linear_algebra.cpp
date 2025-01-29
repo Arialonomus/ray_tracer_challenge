4c4cf3c3abea11366422efa72260ec21d9bffd52
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-29 10:27:41 -0600

    Added generalized submatrix() function for getting flattened submatrices from flattened matrices

---
+++ src/graphics/functions/linear_algebra.cpp
@@ -0,0 +1,23 @@
+#include "linear_algebra.hpp"
+
+#include "cmath"
+
+namespace gfx{
+    std::vector<float> submatrix(const std::vector<float>& matrix_values,
+                                 const size_t row_to_remove,
+                                 const size_t col_to_remove)
+    {
+        const size_t matrix_dimension{ static_cast<size_t>(std::sqrt(matrix_values.size())) };
+        const size_t submatrix_dimension{ matrix_dimension - 1 };
+        std::vector<float> submatrix_values(submatrix_dimension * submatrix_dimension);
+
+        for (int row = 0; row < submatrix_dimension; ++row)
+            for (int col = 0; col < submatrix_dimension; ++col) {
+                submatrix_values[row * submatrix_dimension + col] =
+                        matrix_values[(row + (row >= row_to_remove ? 1 : 0)) * matrix_dimension +
+                        (col + (col >= col_to_remove ? 1 : 0))];
+            }
+
+        return submatrix_values;
+    }
+}
---
+++ src/graphics/functions/linear_algebra.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+namespace gfx {
+    /* Flattened Matrix Operations */
+
+    // Returns a flattened square matrix of dimension n-1 calculated from a matrix with dimension n
+    // with the specified row and column removed
+    [[nodiscard]] std::vector<float> submatrix(const std::vector<float>& matrix_values,
+                                               size_t row_to_remove,
+                                               size_t col_to_remove);
+}
