3660f955855f504fe662f0fa595efa0cf411188d
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-30 18:17:39 -0600

    Added tests for basic stripe pattern operations

--- src/graphics/shading/patterns/pattern.test.cpp
+++ src/graphics/shading/patterns/pattern.test.cpp
@@ -4,6 +4,9 @@
 
 #include "matrix4.hpp"
 #include "transform.hpp"
+#include "color.hpp"
+
+/* Pattern Base Class */
 
 class TestPattern : public gfx::Pattern
 {
@@ -56,4 +59,71 @@ TEST(GraphicsPattern, SetTransform)
     pattern.setTransform(transform_expected);
 
     ASSERT_EQ(pattern.getTransform(), transform_expected);
+}
+
+/* StripePattern Derived Class */
+
+// Tests the color constructor for the stripe pattern class
+TEST(GraphicsPattern, StripePatternColorConstructor)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createIdentityMatrix() };
+    const gfx::Color color_a_expected{ gfx::black() };
+    const gfx::Color color_b_expected{ gfx::white() };
+    const gfx::StripePattern stripe_pattern{ color_a_expected,
+                                             color_b_expected };
+
+    ASSERT_EQ(stripe_pattern.getTransform(), transform_expected);
+    ASSERT_EQ(stripe_pattern.getColorA(), color_a_expected);
+    ASSERT_EQ(stripe_pattern.getColorB(), color_b_expected);
+}
+
+// Tests the standard constructor for the stripe pattern class
+TEST(GraphicsPattern, StripePatternStandardConstructor)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createTranslationMatrix(1, 2, 3) };
+    const gfx::Color color_a_expected{ gfx::black() };
+    const gfx::Color color_b_expected{ gfx::white() };
+    const gfx::StripePattern stripe_pattern{ transform_expected,
+                                             color_a_expected,
+                                             color_b_expected };
+
+    ASSERT_EQ(stripe_pattern.getTransform(), transform_expected);
+    ASSERT_EQ(stripe_pattern.getColorA(), color_a_expected);
+    ASSERT_EQ(stripe_pattern.getColorB(), color_b_expected);
+}
+
+// Tests the copy constructor for the stripe pattern class
+TEST(GraphicsPattern, StripePatternCopyConstructor)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createTranslationMatrix(1, 2, 3) };
+    const gfx::Color color_a_expected{ gfx::black() };
+    const gfx::Color color_b_expected{ gfx::white() };
+    const gfx::StripePattern stripe_pattern_src{ transform_expected,
+                                                 color_a_expected,
+                                                 color_b_expected };
+    const gfx::StripePattern stripe_pattern_cpy{ stripe_pattern_src };
+
+    ASSERT_EQ(stripe_pattern_cpy.getTransform(), transform_expected);
+    ASSERT_EQ(stripe_pattern_cpy.getColorA(), color_a_expected);
+    ASSERT_EQ(stripe_pattern_cpy.getColorB(), color_b_expected);
+}
+
+// Tests the assignment operator for the stripe pattern class
+TEST(GraphicsPattern, StripePatternAssignmentOperator)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createTranslationMatrix(1, 2, 3) };
+    const gfx::Color color_a_expected{ gfx::black() };
+    const gfx::Color color_b_expected{ gfx::white() };
+    const gfx::StripePattern stripe_pattern_a{ transform_expected,
+                                               color_a_expected,
+                                               color_b_expected };
+    gfx::StripePattern stripe_pattern_b{ gfx::createIdentityMatrix(),
+                                         gfx::red(),
+                                         gfx::blue() };
+
+    stripe_pattern_b = stripe_pattern_a;
+
+    ASSERT_EQ(stripe_pattern_b.getTransform(), transform_expected);
+    ASSERT_EQ(stripe_pattern_b.getColorA(), color_a_expected);
+    ASSERT_EQ(stripe_pattern_b.getColorB(), color_b_expected);
 }
