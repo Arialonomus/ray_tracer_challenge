e23f8356a84d39604a64ba4b298cbff8dde7e94c
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-31 10:59:36 -0600

    Added m_pattern member and updated constructors and equality operator to accommodate

--- src/graphics/shading/material.cpp
+++ src/graphics/shading/material.cpp
@@ -8,9 +8,20 @@ namespace gfx {
     {
         return
             m_color == rhs.getColor() &&
+            ((!m_pattern && !rhs.hasPattern()) || (m_pattern && rhs.hasPattern() && *m_pattern == rhs.getPattern())) &&
             utils::areEqual(m_ambient, rhs.getAmbient()) &&
             utils::areEqual(m_diffuse, rhs.getDiffuse()) &&
             utils::areEqual(m_specular, rhs.getSpecular()) &&
             utils::areEqual(m_shininess, rhs.getShininess());
+    }
+
+    // Pattern Mutator
+    void Material::setPattern(const Pattern& pattern)
+    {
+        if (m_pattern) {
+            *m_pattern = pattern;
+        } else {
+            m_pattern = pattern.clone();
+        }
     }
 }
--- src/graphics/shading/material.hpp
+++ src/graphics/shading/material.hpp
@@ -1,24 +1,67 @@
 #pragma once
 
+#include <memory>
+
 #include "color.hpp"
+#include "pattern.hpp"
 
 namespace gfx {
-    class Material {
+    class Material
+    {
     public:
         /* Constructors */
 
+        // Default Constructor
         Material() = default;
+
+        // Color-Only Constructor
+        explicit Material(const Color& color)
+                : m_color{ color },
+                  m_pattern{ nullptr },
+                  m_ambient{ 0.1 },
+                  m_diffuse{ 0.9 },
+                  m_specular{ 0.9 },
+                  m_shininess{ 200 }
+        {}
+
+        // Pattern-Only Constructor
+        explicit Material(const Pattern& pattern)
+                : m_color{ 1, 1, 1 },
+                  m_pattern{ pattern.clone() },
+                  m_ambient{ 0.1 },
+                  m_diffuse{ 0.9 },
+                  m_specular{ 0.9 },
+                  m_shininess{ 200 }
+        {}
+
+        // Phong Values-Only Constructor
+        Material(const double ambient,
+                 const double diffuse,
+                 const double specular,
+                 const double shininess)
+                : m_color{ 1, 1, 1 },
+                  m_pattern{ nullptr },
+                  m_ambient{ ambient },
+                  m_diffuse{ diffuse },
+                  m_specular{ specular },
+                  m_shininess{ shininess }
+        {}
+
+        // No-Pattern Constructor (Color Object)
         Material(const Color& color,
                  const double ambient,
                  const double diffuse,
                  const double specular,
                  const double shininess)
                 : m_color{ color },
-                m_ambient{ ambient },
-                m_diffuse{ diffuse },
-                m_specular{ specular },
-                m_shininess{ shininess }
+                  m_pattern{ nullptr },
+                  m_ambient{ ambient },
+                  m_diffuse{ diffuse },
+                  m_specular{ specular },
+                  m_shininess{ shininess }
         {}
+
+        // No-Pattern Constructor (Float List)
         Material(const double color_r,
                  const double color_g,
                  const double color_b,
@@ -26,57 +69,124 @@ namespace gfx {
                  const double diffuse,
                  const double specular,
                  const double shininess)
-                : m_color{ Color{ color_r, color_g, color_b } },
+                : m_color{ Color{ color_r, color_g, color_b }},
+                  m_pattern{ nullptr },
                   m_ambient{ ambient },
                   m_diffuse{ diffuse },
                   m_specular{ specular },
                   m_shininess{ shininess }
         {}
-        explicit Material(const Color& color)
-                : m_color{ color },
-                  m_ambient{ 0.1 },
-                  m_diffuse{ 0.9 },
-                  m_specular{ 0.9 },
-                  m_shininess{ 200 }
-        {}
-        Material(const double ambient,
+
+        // No-Color Constructor
+        Material(const Pattern& pattern,
+                 const double ambient,
                  const double diffuse,
                  const double specular,
                  const double shininess)
                 : m_color{ 1, 1, 1 },
+                  m_pattern{ pattern.clone() },
                   m_ambient{ ambient },
                   m_diffuse{ diffuse },
                   m_specular{ specular },
                   m_shininess{ shininess }
         {}
-        Material(const Material&) = default;
-        Material(Material&&) = default;
+
+        // Copy Constructor
+        Material(const Material& src)
+                : m_color{ src.m_color },
+                  m_pattern{ src.m_pattern ? src.m_pattern->clone() : nullptr },
+                  m_ambient{ src.m_ambient },
+                  m_diffuse{ src.m_diffuse },
+                  m_specular{ src.m_specular },
+                  m_shininess{ src.m_shininess }
+        {}
+
+        // Move Constructor
+        Material(Material&& src)
+                : m_color{ std::move(src.m_color) },
+                  m_pattern{ std::move(src.m_pattern) },
+                  m_ambient{ src.m_ambient },
+                  m_diffuse{ src.m_diffuse },
+                  m_specular{ src.m_specular },
+                  m_shininess{ src.m_shininess }
+        {}
 
         /* Destructor */
 
-        virtual ~Material() = default;
+        ~Material() = default;
 
         /* Assignment Operators */
 
-        Material& operator=(const Material&) = default;
-        Material& operator=(Material&&) = default;
+        // Copy Assignment Operator
+        Material& operator=(const Material& src)
+        {
+            m_color = src.m_color;
+            m_pattern = src.m_pattern ? src.m_pattern->clone() : nullptr;
+            m_ambient = src.m_ambient;
+            m_diffuse = src.m_diffuse;
+            m_specular = src.m_specular;
+            m_shininess = src.m_shininess;
+
+            return *this;
+        }
+
+        // Move Assignment Operator
+        Material& operator=(Material&& src)
+        {
+            m_color = std::move(src.m_color);
+            m_pattern = std::move(src.m_pattern);
+            m_ambient = src.m_ambient;
+            m_diffuse = src.m_diffuse;
+            m_specular = src.m_specular;
+            m_shininess = src.m_shininess;
+
+            return *this;
+        }
 
         /* Accessors */
 
-        [[nodiscard]] const Color& getColor() const { return m_color; }
-        [[nodiscard]] double getAmbient() const { return m_ambient; }
-        [[nodiscard]] double getDiffuse() const { return m_diffuse; }
-        [[nodiscard]] double getSpecular() const { return m_specular; }
-        [[nodiscard]] double getShininess() const { return m_shininess; }
+        [[nodiscard]] const Color& getColor() const
+        { return m_color; }
+
+        [[nodiscard]] bool hasPattern() const
+        { return m_pattern != nullptr; }
+
+        [[nodiscard]] const Pattern& getPattern() const
+        { return *m_pattern; }
+
+        [[nodiscard]] double getAmbient() const
+        { return m_ambient; }
+
+        [[nodiscard]] double getDiffuse() const
+        { return m_diffuse; }
+
+        [[nodiscard]] double getSpecular() const
+        { return m_specular; }
+
+        [[nodiscard]] double getShininess() const
+        { return m_shininess; }
 
         /* Mutators */
 
-        void setColor(const Color& color) { m_color = color; }
-        void setColor(const double r, const double g, const double b) { m_color.setValues(r, g, b); }
-        void setAmbient(const double ambient) { m_ambient = ambient; }
-        void setDiffuse(const double diffuse) { m_diffuse = diffuse; }
-        void setSpecular(const double specular) { m_specular = specular; }
-        void setShininess(const double shininess) { m_shininess = shininess; }
+        void setColor(const Color& color)
+        { m_color = color; }
+
+        void setColor(const double r, const double g, const double b)
+        { m_color.setValues(r, g, b); }
+
+        void setPattern(const Pattern& pattern);
+
+        void setAmbient(const double ambient)
+        { m_ambient = ambient; }
+
+        void setDiffuse(const double diffuse)
+        { m_diffuse = diffuse; }
+
+        void setSpecular(const double specular)
+        { m_specular = specular; }
+
+        void setShininess(const double shininess)
+        { m_shininess = shininess; }
 
         /* Comparison Operator Overloads */
 
@@ -86,6 +196,7 @@ namespace gfx {
         /* Data Members */
 
         Color m_color{ 1, 1, 1 };
+        std::unique_ptr<Pattern> m_pattern{ nullptr };
         double m_ambient{ 0.1 };
         double m_diffuse{ 0.9 };
         double m_specular{ 0.9 };
