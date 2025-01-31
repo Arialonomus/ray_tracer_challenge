a4c4d58a73be16abbb2167f8ecdfbbdb55c547e4
Jacob Barber <barber.jacobervin@gmail.com>
2025-01-31 10:59:51 -0600

    Updated test suite to account for m_pattern member

--- src/graphics/shading/material.test.cpp
+++ src/graphics/shading/material.test.cpp
@@ -2,6 +2,8 @@
 #include "material.hpp"
 
 #include "color.hpp"
+#include "stripe_pattern.hpp"
+#include "transform.hpp"
 
 // Tests the default constructor
 TEST(GraphicsMaterial, DefaultConstructor)
@@ -14,60 +16,14 @@ TEST(GraphicsMaterial, DefaultConstructor)
     const double shininess_expected{ 200 };
 
     ASSERT_EQ(material.getColor(), color_expected);
+    ASSERT_FALSE(material.hasPattern());
     ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
     ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
     ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
     ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
 }
 
-// Tests the standard constructor (color object & double list)
-TEST(GraphicsMaterial, StandardConstructor)
-{
-    const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
-    const double ambient_expected{ 0.5 };
-    const double diffuse_expected{ 0.5 };
-    const double specular_expected{ 0.5 };
-    const double shininess_expected{ 50 };
-    const gfx::Material material{ color_expected,
-                                  ambient_expected,
-                                  diffuse_expected,
-                                  specular_expected,
-                                  shininess_expected };
-
-    ASSERT_EQ(material.getColor(), color_expected);
-    ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
-    ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
-    ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
-    ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
-}
-
-// Tests the standard constructor (double list)
-TEST(GraphicsMaterial, StandardFloatListConstructor)
-{
-    const double color_r{ 0.5 };
-    const double color_g{ 0.5 };
-    const double color_b{ 0.5 };
-    const gfx::Color color_expected{ color_r, color_g, color_b };
-    const double ambient_expected{ 0.5 };
-    const double diffuse_expected{ 0.5 };
-    const double specular_expected{ 0.5 };
-    const double shininess_expected{ 50 };
-    const gfx::Material material{ color_r,
-                                  color_g,
-                                  color_b,
-                                  ambient_expected,
-                                  diffuse_expected,
-                                  specular_expected,
-                                  shininess_expected };
-
-    ASSERT_EQ(material.getColor(), color_expected);
-    ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
-    ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
-    ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
-    ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
-}
-
-// Tests the color constructor
+// Tests the color-only constructor
 TEST(GraphicsMaterial, ColorConstructor)
 {
     const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
@@ -78,14 +34,36 @@ TEST(GraphicsMaterial, ColorConstructor)
     const gfx::Material material{ color_expected };
 
     ASSERT_EQ(material.getColor(), color_expected);
+    ASSERT_FALSE(material.hasPattern());
     ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
     ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
     ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
     ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
 }
 
-// Tests the attribute list constructor
-TEST(GraphicsMaterial, AttributeList)
+// Tests the pattern-only constructor
+TEST(GraphicsMaterial, PatternOnlyConstructor)
+{
+    const gfx::Color color_expected{ 1, 1, 1 };
+    const gfx::StripePattern pattern_expected{ gfx::createScalingMatrix(5),
+                                               gfx::white(),
+                                               gfx::black() };
+    const double ambient_expected{ 0.1 };
+    const double diffuse_expected{ 0.9 };
+    const double specular_expected{ 0.9 };
+    const double shininess_expected{ 200 };
+    const gfx::Material material{ pattern_expected };
+
+    ASSERT_EQ(material.getColor(), color_expected);
+    ASSERT_EQ(material.getPattern(), pattern_expected);
+    ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
+    ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
+    ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
+    ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
+}
+
+// Tests the Phong values list-only constructor
+TEST(GraphicsMaterial, PhongValuesList)
 {
     const gfx::Color color_expected{ 1, 1, 1 };
     const double ambient_expected{ 0.5 };
@@ -98,6 +76,74 @@ TEST(GraphicsMaterial, AttributeList)
                                   shininess_expected };
 
     ASSERT_EQ(material.getColor(), color_expected);
+    ASSERT_FALSE(material.hasPattern());
+    ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
+    ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
+    ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
+    ASSERT_FLOAT_EQ(material.getShininess(), shininess_expected);
+}
+
+// Tests the no-pattern constructors
+TEST(GraphicsMaterial, NoPatternConstructors)
+{
+    // Test the color object constructor
+    const gfx::Color color_a_expected{ 0.5, 0.5, 0.5 };
+    const double ambient_expected{ 0.5 };
+    const double diffuse_expected{ 0.5 };
+    const double specular_expected{ 0.5 };
+    const double shininess_expected{ 50 };
+    const gfx::Material material_a{ color_a_expected,
+                                    ambient_expected,
+                                    diffuse_expected,
+                                    specular_expected,
+                                    shininess_expected };
+
+    ASSERT_EQ(material_a.getColor(), color_a_expected);
+    ASSERT_FLOAT_EQ(material_a.getAmbient(), ambient_expected);
+    ASSERT_FLOAT_EQ(material_a.getDiffuse(), diffuse_expected);
+    ASSERT_FLOAT_EQ(material_a.getSpecular(), specular_expected);
+    ASSERT_FLOAT_EQ(material_a.getShininess(), shininess_expected);
+
+    // Test the float list constructor
+    const double color_r{ 0.5 };
+    const double color_g{ 0.5 };
+    const double color_b{ 0.5 };
+    const gfx::Color color_b_expected{ color_r, color_g, color_b };
+    const gfx::Material material_b{ color_r,
+                                    color_g,
+                                    color_b,
+                                    ambient_expected,
+                                    diffuse_expected,
+                                    specular_expected,
+                                    shininess_expected };
+
+    ASSERT_EQ(material_b.getColor(), color_b_expected);
+    ASSERT_FLOAT_EQ(material_b.getAmbient(), ambient_expected);
+    ASSERT_FLOAT_EQ(material_b.getDiffuse(), diffuse_expected);
+    ASSERT_FLOAT_EQ(material_b.getSpecular(), specular_expected);
+    ASSERT_FLOAT_EQ(material_b.getShininess(), shininess_expected);
+}
+
+// Tests the no-color constructor
+TEST(GraphicsMaterial, NoColorConstructor)
+{
+    // Test the color object constructor
+    const gfx::Color color_expected{ 1, 1, 1 };
+    const gfx::StripePattern pattern_expected{ gfx::createScalingMatrix(5),
+                                               gfx::white(),
+                                               gfx::black() };
+    const double ambient_expected{ 0.5 };
+    const double diffuse_expected{ 0.5 };
+    const double specular_expected{ 0.5 };
+    const double shininess_expected{ 50 };
+    const gfx::Material material{ pattern_expected,
+                                  ambient_expected,
+                                  diffuse_expected,
+                                  specular_expected,
+                                  shininess_expected };
+
+    ASSERT_EQ(material.getColor(), color_expected);
+    ASSERT_EQ(material.getPattern(), pattern_expected);
     ASSERT_FLOAT_EQ(material.getAmbient(), ambient_expected);
     ASSERT_FLOAT_EQ(material.getDiffuse(), diffuse_expected);
     ASSERT_FLOAT_EQ(material.getSpecular(), specular_expected);
@@ -188,6 +234,12 @@ TEST(GraphicsMaterial, Mutators)
     const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
     material.setColor(color_expected);
     ASSERT_EQ(material.getColor(), color_expected);
+
+    const gfx::StripePattern pattern_expected{ gfx::createScalingMatrix(5),
+                                               gfx::white(),
+                                               gfx::black() };
+    material.setPattern(pattern_expected);
+    ASSERT_EQ(material.getPattern(), pattern_expected);
 
     const double ambient_expected{ 0.5 };
     material.setAmbient(ambient_expected);
