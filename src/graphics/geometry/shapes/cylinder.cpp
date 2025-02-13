0f69147f2042a9b3c03ecbc11a26c952093703e9
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-13 10:50:50 -0600

    Implemented calculateIntersections() to handle intersections with unbounded cylinders

--- src/graphics/geometry/shapes/cylinder.cpp
+++ src/graphics/geometry/shapes/cylinder.cpp
@@ -20,6 +20,27 @@ namespace gfx {
     // Ray-Cylinder Intersection Calculator
     std::vector<Intersection> Cylinder::calculateIntersections(const Ray& transformed_ray) const
     {
-        return std::vector<Intersection>{ };
+        const Vector4 direction{ transformed_ray.getDirection() };
+        const Vector4 origin{ transformed_ray.getOrigin() };
+
+        const double a{ std::pow(direction.x(), 2) + std::pow(direction.z(), 2) };
+        if (utils::areEqual(a, 0.0)) {
+            // Ray is parallel to the y-axis
+            return std::vector<Intersection>{ };
+        }
+
+        const double b{ (2 * origin.x() * direction.x()) + (2 * origin.z() * direction.z()) };
+        const double c{ std::pow(origin.x(), 2) + std::pow(origin.z(), 2) - 1 };
+
+        const double discriminant{ std::pow(b, 2) - (4 * a * c) };
+        if (utils::isLess(discriminant, 0.0)) {
+            // Ray misses the cylinder
+            return std::vector<Intersection>{ };
+        }
+
+        const double t_0{ (-b - std::sqrt(discriminant)) / (2 * a) };
+        const double t_1{ (-b + std::sqrt(discriminant)) / (2 * a) };
+
+        return std::vector<Intersection>{ Intersection{ t_0, this }, Intersection{ t_1, this } };
     }
 }
