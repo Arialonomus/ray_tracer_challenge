f7fa49d92a8b52faf6ca087a744107c21a18be54
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-13 12:45:10 -0600

    Added m_y_min and m_y_max data members and associated constructors and methods

--- src/graphics/geometry/shapes/cylinder.hpp
+++ src/graphics/geometry/shapes/cylinder.hpp
@@ -3,6 +3,7 @@
 #include "shape.hpp"
 
 #include <vector>
+#include <limits>
 
 #include "matrix4.hpp"
 #include "vector4.hpp"
@@ -16,16 +17,59 @@ namespace gfx {
     public:
         /* Constructors */
 
+        // Default Constructor (Unbounded Cylinder)
         Cylinder() = default;
+
+        // Default Constructor (Bounded Cylinder)
+        Cylinder(const double y_min, const double y_max)
+                : Shape{ },
+                  m_y_min{ y_min },
+                  m_y_max{ y_max }
+        {}
+
+        // Transform-Only Constructor (Unbounded Cylinder)
         explicit Cylinder(const Matrix4& transform)
-                : Shape{ transform }
+                : Shape{ transform },
+                  m_y_min{ -std::numeric_limits<double>::infinity() },
+                  m_y_max{ std::numeric_limits<double>::infinity() }
         {}
+
+        // Transform-Only Constructor (Bounded Cylinder)
+        Cylinder(const Matrix4& transform, const double y_min, const double y_max)
+                : Shape{ transform },
+                  m_y_min{ y_min },
+                  m_y_max{ y_max }
+        {}
+
+        // Material-Only Constructor (Unbounded Cylinder)
         explicit Cylinder(const Material& material)
-                : Shape{ material }
+                : Shape{ material },
+                  m_y_min{ -std::numeric_limits<double>::infinity() },
+                  m_y_max{ std::numeric_limits<double>::infinity() }
         {}
+
+        // Material-Only Constructor (Bounded Cylinder)
+        Cylinder(const Material& material, const double y_min, const double y_max)
+                : Shape{ material },
+                  m_y_min{ y_min },
+                  m_y_max{ y_max }
+        {}
+
+        // Standard Constructor (Unbounded Cylinder)
         Cylinder(const Matrix4& transform, const Material& material)
-                : Shape{ transform, material }
+                : Shape{ transform, material },
+                  m_y_min{ -std::numeric_limits<double>::infinity() },
+                  m_y_max{ std::numeric_limits<double>::infinity() }
         {}
+
+        // Standard Constructor (Bounded Cylinder)
+        Cylinder(const Matrix4& transform, const Material& material, const double y_min, const double y_max)
+                : Shape{ transform, material },
+                  m_y_min{ y_min },
+                  m_y_max{ y_max }
+        {}
+
+        // Copy Constructor
         Cylinder(const Cylinder&) = default;
 
         /* Destructor */
@@ -36,6 +80,28 @@ namespace gfx {
 
         Cylinder& operator=(const Cylinder&) = default;
 
+        /* Accessors */
+
+        [[nodiscard]] double getYMin() const
+        { return m_y_min; }
+
+        [[nodiscard]] double getYMax() const
+        { return m_y_max; }
+
+        /* Mutators */
+
+        void setYMin(const double y_min)
+        { m_y_min = y_min; }
+
+        void uncapYMin()
+        { m_y_min = -std::numeric_limits<double>::infinity(); }
+
+        void setYMax(const double y_max)
+        { m_y_max = y_max; }
+
+        void uncapYMax()
+        { m_y_max = std::numeric_limits<double>::infinity(); }
+
         /* Comparison Operator Overloads */
 
         [[nodiscard]] bool operator==(const Cylinder& rhs) const;
@@ -45,6 +111,11 @@ namespace gfx {
         { return std::make_shared<Cylinder>(*this); }
 
     private:
+        /* Data Members */
+
+        double m_y_min{ -std::numeric_limits<double>::infinity() };
+        double m_y_max{ std::numeric_limits<double>::infinity() };
+
         /* Helper Method Overrides */
 
         [[nodiscard]] Vector4 calculateSurfaceNormal(const Vector4& transformed_point) const override;
