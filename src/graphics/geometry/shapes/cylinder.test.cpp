1146c33e71219872574d3f4c00128d9e7b9e1bac
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-13 10:50:27 -0600

    Added tests for ray-cylinder hits with an unbounded cylinder

--- src/graphics/geometry/shapes/cylinder.test.cpp
+++ src/graphics/geometry/shapes/cylinder.test.cpp
@@ -103,7 +103,7 @@ TEST(GraphicsCylinder, InequalityOperator)
 // Tests a ray missing a cylinder
 TEST(GraphicsCylinder, RayCylinderMisses)
 {
-    const gfx::Cylinder cylinder{ };    // Assume an axis-aligned bounding box
+    const gfx::Cylinder cylinder{ };
 
     const std::vector<gfx::Vector4> origin_list{
             gfx::createPoint(1, 0, 0),
@@ -121,10 +121,48 @@ TEST(GraphicsCylinder, RayCylinderMisses)
 
     for (int i = 0; i < origin_list.size(); ++i) {
         const gfx::Ray ray{ origin_list[i],
-                            direction_list[i] };
+                            gfx::normalize(direction_list[i]) };
 
         std::vector<gfx::Intersection> intersections{ cylinder.getObjectIntersections(ray) };
 
         EXPECT_TRUE(intersections.empty());
+    }
+}
+
+// Tests ray intersections with an unbounded cylinder
+TEST(GraphicsCylinder, RayCylinderHitsUnbounded)
+{
+    const gfx::Cylinder cylinder{ };
+
+    const std::vector<gfx::Vector4> origin_list{
+            gfx::createPoint(1, 0, -5),
+            gfx::createPoint(0, 0, -5),
+            gfx::createPoint(0.5, 0, -5),
+    };
+
+    const std::vector<gfx::Vector4> direction_list{
+            gfx::createVector(0, 0, 1),
+            gfx::createVector(0, 0, 1),
+            gfx::createVector(0.1, 1, 1),
+    };
+
+    const std::vector<std::pair<double, double>> intersection_t_expected_list {
+            { 5, 5 },
+            { 4, 6 },
+            { 6.80798, 7.088724 }
+    };
+
+    ASSERT_TRUE(origin_list.size() == direction_list.size());
+    ASSERT_TRUE(origin_list.size() == intersection_t_expected_list.size());
+
+    for (int i = 0; i < origin_list.size(); ++i) {
+        const gfx::Ray ray{ origin_list[i],
+                            gfx::normalize(direction_list[i]) };
+
+        std::vector<gfx::Intersection> intersections{ cylinder.getObjectIntersections(ray) };
+
+        const auto [ t1_expected, t2_expected ] { intersection_t_expected_list[i] };
+        EXPECT_FLOAT_EQ(intersections[0].getT(), t1_expected);
+        EXPECT_FLOAT_EQ(intersections[1].getT(), t2_expected);
     }
 }
