5bc386c46cb993fd8c2159e265b9150ec253db6c
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-17 12:24:29 -0600

    Added basic tests for Cone class

---
+++ src/graphics/geometry/shapes/cone.test.cpp
@@ -0,0 +1,258 @@
+#include "gtest/gtest.h"
+#include "cone.hpp"
+
+#include <vector>
+
+#include "matrix4.hpp"
+#include "material.hpp"
+#include "transform.hpp"
+#include "ray.hpp"
+
+// Tests the default constructors
+TEST(GraphicsCone, DefaultConstructor)
+{
+    const gfx::Cone cone_unbounded{ };
+    const gfx::Matrix4 transform_expected{ gfx::createIdentityMatrix() };
+
+    // Test an unbounded cone
+    const gfx::Material material_expected{ };
+
+    ASSERT_EQ(cone_unbounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_unbounded.getMaterial(), material_expected);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMin()));
+    ASSERT_TRUE(cone_unbounded.getYMin() < 0);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMax()));
+    ASSERT_TRUE(cone_unbounded.getYMax() > 0);
+    ASSERT_FALSE(cone_unbounded.isClosed());
+
+    // Test a bounded cone
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+    const gfx::Cone cone_bounded{ y_min_expected, y_max_expected };
+
+    ASSERT_EQ(cone_bounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_bounded.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMax(), y_max_expected);
+    ASSERT_FALSE(cone_bounded.isClosed());
+}
+
+// Tests the material-only constructors
+TEST(GraphicsCone, StandardConstructorDefaultMaterial)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createScalingMatrix(5) };
+    const gfx::Material material_expected{ };
+
+    // Test an unbounded cone
+    const gfx::Cone cone_unbounded{ transform_expected };
+
+    ASSERT_EQ(cone_unbounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_unbounded.getMaterial(), material_expected);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMin()));
+    ASSERT_TRUE(cone_unbounded.getYMin() < 0);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMax()));
+    ASSERT_TRUE(cone_unbounded.getYMax() > 0);
+    ASSERT_FALSE(cone_unbounded.isClosed());
+
+    // Test a bounded cone
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+    const gfx::Cone cone_bounded{ transform_expected, y_min_expected, y_max_expected };
+
+    ASSERT_EQ(cone_bounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_bounded.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMax(), y_max_expected);
+    ASSERT_FALSE(cone_bounded.isClosed());
+}
+
+// Tests the transform-only constructors
+TEST(GraphicsCone, StandardConstructorDefaultTransform)
+{
+    const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
+    const gfx::Material material_expected{ color_expected, 0.5, 0.5, 0.5, 50, 0.5 };
+    const gfx::Matrix4 transform_expected{ gfx::createIdentityMatrix() };
+
+    // Test an unbounded cone
+    const gfx::Cone cone_unbounded{ material_expected };
+
+    ASSERT_EQ(cone_unbounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_unbounded.getMaterial(), material_expected);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMin()));
+    ASSERT_TRUE(cone_unbounded.getYMin() < 0);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMax()));
+    ASSERT_TRUE(cone_unbounded.getYMax() > 0);
+    ASSERT_FALSE(cone_unbounded.isClosed());
+
+    // Test a bounded cone
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+    const gfx::Cone cone_bounded{ material_expected, y_min_expected, y_max_expected };
+
+    ASSERT_EQ(cone_bounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_bounded.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMax(), y_max_expected);
+    ASSERT_FALSE(cone_bounded.isClosed());
+}
+
+// Tests the standard constructors
+TEST(GraphicsCone, StandardConstructor)
+{
+    const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
+    const gfx::Material material_expected{ color_expected, 0.5, 0.5, 0.5, 50, 0.5 };
+    const gfx::Matrix4 transform_expected{ gfx::createScalingMatrix(5) };
+
+    // Test an unbounded cone
+    const gfx::Cone cone_unbounded{ transform_expected, material_expected };
+
+    ASSERT_EQ(cone_unbounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_unbounded.getMaterial(), material_expected);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMin()));
+    ASSERT_TRUE(cone_unbounded.getYMin() < 0);
+    ASSERT_TRUE(std::isinf(cone_unbounded.getYMax()));
+    ASSERT_TRUE(cone_unbounded.getYMax() > 0);
+    ASSERT_FALSE(cone_unbounded.isClosed());
+
+    // Test a bounded cone (with end caps)
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+    const gfx::Cone cone_bounded{ transform_expected,
+                                          material_expected,
+                                          y_min_expected,
+                                          y_max_expected,
+                                          true };
+
+    ASSERT_EQ(cone_bounded.getTransform(), transform_expected);
+    ASSERT_EQ(cone_bounded.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_bounded.getYMax(), y_max_expected);
+    ASSERT_TRUE(cone_bounded.isClosed());
+}
+
+// Tests the copy constructor
+TEST(GraphicsCone, CopyConstructor)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createScalingMatrix(5) };
+    const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
+    const gfx::Material material_expected{ color_expected, 0.5, 0.5, 0.5, 50, 0.5 };
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+
+    const gfx::Cone cone_src{ transform_expected,
+                                      material_expected,
+                                      y_min_expected,
+                                      y_max_expected };
+    const gfx::Cone cone_cpy{ cone_src };
+
+    ASSERT_EQ(cone_cpy.getTransform(), transform_expected);
+    ASSERT_EQ(cone_cpy.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_cpy.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_cpy.getYMax(), y_max_expected);
+    ASSERT_FALSE(cone_cpy.isClosed());
+}
+
+// Tests the assignment operator
+TEST(GraphicsCone, AssignmentOperator)
+{
+    const gfx::Matrix4 transform_expected{ gfx::createScalingMatrix(5) };
+    const gfx::Color color_expected{ 0.5, 0.5, 0.5 };
+    const gfx::Material material_expected{ color_expected, 0.5, 0.5, 0.5, 50, 0.5 };
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+
+    const gfx::Cone cone_a{ transform_expected,
+                                    material_expected,
+                                    y_min_expected,
+                                    y_max_expected };
+    gfx::Cone cone_b{ };
+
+    cone_b = cone_a;
+
+    ASSERT_EQ(cone_b.getTransform(), transform_expected);
+    ASSERT_EQ(cone_b.getMaterial(), material_expected);
+    ASSERT_FLOAT_EQ(cone_b.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone_b.getYMax(), y_max_expected);
+    ASSERT_FALSE(cone_b.isClosed());
+}
+
+// Tests the mutators
+TEST(GraphicsCone, Mutators)
+{
+    gfx::Cone cone{ };
+
+    // Test setting y_min and y_max with values
+    const double y_min_expected{ -1 };
+    const double y_max_expected{ 1 };
+
+    cone.setYMin(y_min_expected);
+    ASSERT_FLOAT_EQ(cone.getYMin(), y_min_expected);
+
+    cone.setYMax(y_max_expected);
+    ASSERT_FLOAT_EQ(cone.getYMax(), y_max_expected);
+
+    // Test unbounding y_min and y_max individually
+    cone.unboundYMin();
+    ASSERT_TRUE(std::isinf(cone.getYMin()));
+    ASSERT_TRUE(cone.getYMin() < 0);
+
+    cone.unboundYMax();
+    ASSERT_TRUE(std::isinf(cone.getYMax()));
+    ASSERT_TRUE(cone.getYMax() > 0);
+
+    // Test bounding y_min and y_max at the same time
+    cone.setConeBounds(y_min_expected, y_max_expected);
+    ASSERT_FLOAT_EQ(cone.getYMin(), y_min_expected);
+    ASSERT_FLOAT_EQ(cone.getYMax(), y_max_expected);
+
+    // Test unbounding y_min and y_max at the same time
+    cone.unboundCone();
+    ASSERT_TRUE(std::isinf(cone.getYMin()));
+    ASSERT_TRUE(cone.getYMin() < 0);
+    ASSERT_TRUE(std::isinf(cone.getYMax()));
+    ASSERT_TRUE(cone.getYMax() > 0);
+
+    // Test capping the cone
+    cone.capCone();
+    ASSERT_TRUE(cone.isClosed());
+
+    // Test uncapping the cone
+    cone.uncapCone();
+    ASSERT_FALSE(cone.isClosed());
+}
+
+// Tests the equality operator
+TEST(GraphicsCone, EqualityOperator)
+{
+    const gfx::Cone cone_a{
+            gfx::createScalingMatrix(5),
+            gfx::createGlassyMaterial(),
+            1,
+            2,
+            true
+    };
+    const gfx::Cone cone_b{
+            gfx::createScalingMatrix(5),
+            gfx::createGlassyMaterial(),
+            1,
+            2,
+            true
+    };
+
+    ASSERT_TRUE(cone_a == cone_b);
+}
+
+// Tests the inequality operator
+TEST(GraphicsCone, InequalityOperator)
+{
+    const gfx::Cone cone_a{
+            gfx::createScalingMatrix(5),
+            gfx::createGlassyMaterial(),
+            1,
+            2,
+            true
+    };
+    const gfx::Cone cone_b{ };
+
+    ASSERT_TRUE(cone_a != cone_b);
+}
