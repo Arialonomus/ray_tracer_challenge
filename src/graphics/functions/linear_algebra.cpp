73a8f1ea9bbb46b14132cb8576efc5b3351bbe11
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-26 16:17:31 -0600

    Refactored linear algebra functions to use span arguments

--- src/graphics/functions/linear_algebra.cpp
+++ src/graphics/functions/linear_algebra.cpp
@@ -1,9 +1,9 @@
 #include "linear_algebra.hpp"
 
-#include "cmath"
+#include <cmath>
 
 namespace gfx{
-    std::vector<double> getSubmatrix(const std::vector<double>& matrix_values,
+    std::vector<double> getSubmatrix(std::span<const double> matrix_values,
                                     size_t row_to_remove,
                                     size_t col_to_remove)
     {
@@ -17,27 +17,23 @@ namespace gfx{
                         matrix_values[(row + (row >= row_to_remove ? 1 : 0)) * matrix_dimension +
                         (col + (col >= col_to_remove ? 1 : 0))];
             }
-
         return submatrix_values;
     }
 
-    double calculateDeterminant(const std::vector<double>& matrix_values)
+    double calculateDeterminant(std::span<const double> matrix_values)
     {
         const size_t matrix_dimension{ static_cast<size_t>(std::sqrt(matrix_values.size())) };
 
         // Base Case: 2x2 Matrix
-        if (matrix_dimension == 2) {
+        if (matrix_dimension == 2)
             return matrix_values[0] * matrix_values[3] - matrix_values[1] * matrix_values[2];
-        }
 
         // Recursive Case: Calculate the determinant of the submatrix
-        else {
-            double determinant = 0;
-            for (int col = 0; col < matrix_dimension; ++col) {
-                const double minor = calculateDeterminant(getSubmatrix(matrix_values, 0, col));
-                determinant += matrix_values[col] * (0 + col % 2 == 0 ? minor : -minor);
-            }
-            return determinant;
+        double determinant = 0;
+        for (int col = 0; col < matrix_dimension; ++col) {
+            const double minor = calculateDeterminant(getSubmatrix(matrix_values, 0, col));
+            determinant += matrix_values[col] * (0 + col % 2 == 0 ? minor : -minor);
         }
+        return determinant;
     }
 }
--- src/graphics/functions/linear_algebra.hpp
+++ src/graphics/functions/linear_algebra.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <vector>
+#include <span>
 
 namespace gfx {
     /* Flattened Matrix Operations */
 
     // Returns a flattened square matrix of dimension n-1 calculated from a matrix with dimension n
     // with the specified row and column removed
-    [[nodiscard]] std::vector<double> getSubmatrix(const std::vector<double>& matrix_values,
-                                                  size_t row_to_remove,
-                                                  size_t col_to_remove);
+    [[nodiscard]] std::vector<double> getSubmatrix(std::span<const double> matrix_values,
+                                                   size_t row_to_remove,
+                                                   size_t col_to_remove);
 
     // Recursively calculates the determinant of any square matrix from a vector representing
     // their values stored in flattened, row-major order
-    [[nodiscard]] double calculateDeterminant(const std::vector<double>& matrix_values);
+    [[nodiscard]] double calculateDeterminant(std::span<const double> matrix_values);
 }
