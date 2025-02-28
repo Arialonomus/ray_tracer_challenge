30071cce2f2fa67f1c48b654dfa279a459cbc5fe
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-28 16:09:25 -0600

    Moved ColorTexture tests to texture.test.cpp

--- src/graphics/shading/textures/procedural_textures/procedural_texture.test.cpp
+++ src/graphics/shading/textures/procedural_textures/procedural_texture.test.cpp
@@ -10,31 +10,6 @@
 #include "color_texture.hpp"
 #include "gradient_texture.hpp"
 
-/* Color Texture Derived Class Tests */
-
-// Tests the equality operator for color textures
-TEST(GraphicsProceduralTexture, ColorTextureEqualityOperator)
-{
-    const gfx::Color color{ gfx::cyan() };
-    const gfx::ColorTexture color_texture_a{ color };
-    const gfx::ColorTexture color_texture_b{ color };
-
-    ASSERT_TRUE(color_texture_a == color_texture_b);
-}
-
-// Tests that a color texture returns the correct color when sampled
-TEST(GraphicsProceduralTexture, ColorTextureGetTextureColorAt)
-{
-    const gfx::Color color_expected{ gfx::cyan() };
-    const gfx::ColorTexture color_texture{ color_expected };
-
-    const gfx::Color color_texture_color_actual{
-        color_texture.getTextureColorAt(gfx::createPoint(0.75, 1, 0),
-                                        gfx::ProjectionMap)
-    };
-    EXPECT_EQ(color_texture_color_actual, color_expected);
-}
-
 /* Gradient Texture Derived Class Tests */
 
 // Tests the equality operator for gradient textures
--- src/graphics/shading/textures/texture.test.cpp
+++ src/graphics/shading/textures/texture.test.cpp
@@ -2,6 +2,32 @@
 #pragma ide diagnostic ignored "performance-unnecessary-copy-initialization"
 
 #include "gtest/gtest.h"
-#include "texture.hpp"
+
+#include "color_texture.hpp"
+
+/* Color Texture Derived Class Tests */
+
+// Tests the equality operator for color textures
+TEST(GraphicsTexture, ColorTextureEqualityOperator)
+{
+    const gfx::Color color{ gfx::cyan() };
+    const gfx::ColorTexture color_texture_a{ color };
+    const gfx::ColorTexture color_texture_b{ color };
+
+    ASSERT_TRUE(color_texture_a == color_texture_b);
+}
+
+// Tests that a color texture returns the correct color when sampled
+TEST(GraphicsTexture, ColorTextureGetTextureColorAt)
+{
+    const gfx::Color color_expected{ gfx::cyan() };
+    const gfx::ColorTexture color_texture{ color_expected };
+
+    const gfx::Color color_texture_color_actual{
+            color_texture.getTextureColorAt(gfx::createPoint(0.75, 1, 0),
+                                            gfx::ProjectionMap)
+    };
+    EXPECT_EQ(color_texture_color_actual, color_expected);
+}
 
 #pragma clang diagnostic pop
