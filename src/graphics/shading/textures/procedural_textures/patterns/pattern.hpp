a836887b5cf1822fbf47edffd231734888ab1612
Jacob Barber <barber.jacobervin@gmail.com>
2025-02-28 15:31:33 -0600

    Updated pattern to be derived from ProceduralTexture and moved the TestPatternTex class to the header file for reuse

--- src/graphics/shading/textures/procedural_textures/patterns/pattern.hpp
+++ src/graphics/shading/textures/procedural_textures/patterns/pattern.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include "texture.hpp"
+#include "procedural_texture.hpp"
 #include "color_texture.hpp"
 
 namespace gfx {
-    class Pattern : public Texture
+    class Pattern : public ProceduralTexture
     {
     public:
         /* Constructors */
@@ -14,28 +14,28 @@ namespace gfx {
 
         // Texture-Only Constructor
         Pattern(const Texture& texture_a, const Texture& texture_b)
-                : Texture{ },
+                : ProceduralTexture{ },
                   m_texture_a{ texture_a.clone() },
                   m_texture_b{ texture_b.clone() }
         {}
 
         // Color-Only Constructor
         Pattern(const Color& color_a, const Color& color_b)
-                : Texture{ },
+                : ProceduralTexture{ },
                   m_texture_a{ ColorTexture{ color_a }.clone() },
                   m_texture_b{ ColorTexture{ color_b }.clone() }
         {}
 
         // Standard Constructor (Textures)
-        Pattern(const Matrix3& transform_matrix, const Texture& texture_a, const Texture& texture_b)
-                : Texture{ transform_matrix },
+        Pattern(const Matrix4& transform_matrix, const Texture& texture_a, const Texture& texture_b)
+                : ProceduralTexture{ transform_matrix },
                   m_texture_a{ texture_a.clone() },
                   m_texture_b{ texture_b.clone() }
         {}
 
         // Standard Constructor (Colors)
-        Pattern(const Matrix3& transform_matrix, const Color& color_a, const Color& color_b)
-                : Texture{ transform_matrix },
+        Pattern(const Matrix4& transform_matrix, const Color& color_a, const Color& color_b)
+                : ProceduralTexture{ transform_matrix },
                   m_texture_a{ ColorTexture{ color_a }.clone() },
                   m_texture_b{ ColorTexture{ color_b }.clone() }
         {}
@@ -59,3 +59,42 @@ namespace gfx {
         std::shared_ptr<Texture> m_texture_b{ };
     };
 }
+
+// Concrete pattern implementation used in some test suites to test base class features
+class TestPatternTex : public gfx::Pattern
+{
+public:
+    TestPatternTex()
+            : Pattern{ gfx::white(), gfx::black() }
+    {}
+    TestPatternTex(const gfx::Texture& texture_a, const gfx::Texture& texture_b)
+            : Pattern{ texture_a, texture_b }
+    {}
+    TestPatternTex(const gfx::Color& color_a, const gfx::Color& color_b)
+            : Pattern{ color_a, color_b }
+    {}
+    TestPatternTex(const gfx::Matrix4& transform_matrix, const gfx::Texture& texture_a, const gfx::Texture& texture_b)
+            : Pattern{ transform_matrix, texture_a, texture_b }
+    {}
+    TestPatternTex(const gfx::Matrix4& transform_matrix, const gfx::Color& color_a, const gfx::Color& color_b)
+            : Pattern{ transform_matrix, color_a, color_b }
+    {}
+
+    ~TestPatternTex() override = default;
+
+    [[nodiscard]] std::shared_ptr<Texture> clone() const override
+    { return std::make_shared<TestPatternTex>(*this); }
+
+private:
+    [[nodiscard]] gfx::Color sample3DTextureAt(const gfx::Vector4& transformed_point,
+                                               const gfx::TextureMap& mapping) const override
+    { return { transformed_point.x(), transformed_point.y(), transformed_point.z() }; }
+
+    [[nodiscard]] bool areEquivalent(const Texture& other_texture) const override
+    {
+        const TestPatternTex& other_test_pattern{ dynamic_cast<const TestPatternTex&>(other_texture) };
+        return this->getTransform() == other_test_pattern.getTransform() &&
+               this->getTextureA() == other_test_pattern.getTextureA() &&
+               this->getTextureB() == other_test_pattern.getTextureB();
+    }
+};
